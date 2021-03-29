d0eda66f01f2bef605ee8772825a8b0e6eb3d0b3
Rene Gonzalez <rene_glez1992@hotmail.com>
2021-03-28 19:55:47 -0700

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿
+namespace Actividad_Integradora_2_Problema_6
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtSector = new System.Windows.Forms.TextBox();
+            this.txtEntradas = new System.Windows.Forms.TextBox();
+            this.btnPagar = new System.Windows.Forms.Button();
+            this.lblTotal = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Actividad_Integradora_2_Problema_6.Properties.Resources.Captura;
+            this.pictureBox1.Location = new System.Drawing.Point(69, 42);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(509, 172);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(83, 315);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(117, 41);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Sector: ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(83, 381);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(146, 41);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Entradas: ";
+            // 
+            // txtSector
+            // 
+            this.txtSector.Location = new System.Drawing.Point(224, 312);
+            this.txtSector.Name = "txtSector";
+            this.txtSector.Size = new System.Drawing.Size(250, 47);
+            this.txtSector.TabIndex = 3;
+            // 
+            // txtEntradas
+            // 
+            this.txtEntradas.Location = new System.Drawing.Point(224, 375);
+            this.txtEntradas.Name = "txtEntradas";
+            this.txtEntradas.Size = new System.Drawing.Size(250, 47);
+            this.txtEntradas.TabIndex = 4;
+            // 
+            // btnPagar
+            // 
+            this.btnPagar.Location = new System.Drawing.Point(243, 467);
+            this.btnPagar.Name = "btnPagar";
+            this.btnPagar.Size = new System.Drawing.Size(188, 58);
+            this.btnPagar.TabIndex = 5;
+            this.btnPagar.Text = "Pagar";
+            this.btnPagar.UseVisualStyleBackColor = true;
+            this.btnPagar.Click += new System.EventHandler(this.btnPagar_Click);
+            // 
+            // lblTotal
+            // 
+            this.lblTotal.AutoSize = true;
+            this.lblTotal.Location = new System.Drawing.Point(243, 597);
+            this.lblTotal.Name = "lblTotal";
+            this.lblTotal.Size = new System.Drawing.Size(50, 41);
+            this.lblTotal.TabIndex = 6;
+            this.lblTotal.Text = "$$";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(17F, 41F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(671, 763);
+            this.Controls.Add(this.lblTotal);
+            this.Controls.Add(this.btnPagar);
+            this.Controls.Add(this.txtEntradas);
+            this.Controls.Add(this.txtSector);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Actividad Integradora 2";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtSector;
+        private System.Windows.Forms.TextBox txtEntradas;
+        private System.Windows.Forms.Button btnPagar;
+        private System.Windows.Forms.Label lblTotal;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Actividad_Integradora_2_Problema_6
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            lblTotal.Text = string.Empty;
+        }
+
+        private void btnPagar_Click(object sender, EventArgs e)
+        {
+            double total = 0;
+            switch (txtSector.Text)
+            {
+                case "Sol Candente":
+                    total = Convert.ToInt32(txtEntradas.Text) * 300;
+                    lblTotal.Text = string.Format("Total: ${0} ",total );
+                    break;
+                case "Sol Luminoso":
+                    total = Convert.ToInt32(txtEntradas.Text) * 530;
+                    lblTotal.Text = string.Format("Total: ${0} ", total);
+                    break;
+                case "Sombrita":
+                    total = Convert.ToInt32(txtEntradas.Text) * 610;
+                    lblTotal.Text = string.Format("Total: ${0} ", total);
+                    break;
+                case "Tribuna":
+                    total = Convert.ToInt32(txtEntradas.Text) * 820;
+                    lblTotal.Text = string.Format("Total: ${0} ", total);
+                    break;
+                case "Silla Plastica":
+                    total = Convert.ToInt32(txtEntradas.Text) * 930;
+                    lblTotal.Text = string.Format("Total: ${0} ", total);
+                    break;
+            }
+        }
+    }
+}
